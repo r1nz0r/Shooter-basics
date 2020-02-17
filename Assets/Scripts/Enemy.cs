eb78f2ba7fca62736f5eab63a7dfc4eb074184a5
Aleksei Boitsov <33631903+r1nz0r@users.noreply.github.com>
2020-02-17 21:53:33 +0300

    Начало прототипирования

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Transform player;
+
+    private NavMeshAgent m_navMeshEnemy;
+
+    private void Start()
+    {
+        m_navMeshEnemy = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        m_navMeshEnemy.SetDestination(player.position);
+    }
+}
